356ac5bf8eea53aa3a6c9c16f98305141a58dfd1
Mahesh <maheshh.kumar1508@gmail.com>
2026-07-04 10:12:52 +0530

    chatbot with messages package

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,24 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful assistant")
+]
 
 while True:
     user_input = input("User: ")
-    chat_history.append(user_input)
     if user_input == "exit":
         break
     else:
+        chat_history.append(HumanMessage(content = user_input))
         response = model.invoke(chat_history)
         answer = response.content[0]["text"]
-        chat_history.append(answer)
+        chat_history.append(AIMessage(content=answer))
         print("AI: ",answer)
 
 print(chat_history)
